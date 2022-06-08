22fccb22c1f38818ae2e4e403d88263fd8667139
Aydeke <aydeke2@gmail.com>
2022-06-08 21:40:08 +0600

    Add register.html

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 3000;
+
+app.use(express.json()); // для парсинга application/json
+app.use(express.urlencoded({ extended: true })); // для парсинга application/x-www-form-urlencoded
 
 app.listen(port, () => {
   console.log(`Сервер слушает порт ${port}`);
@@ -29,4 +33,62 @@ app.get("/json", (req, res) => {
 
 app.get("/zapros", (req, res) => {
   res.json(req.query);
+});
+
+app.post("/login", (req, res) => {
+  res.json(req.body);
+});
+
+app.get("/products/:title", (req, res) => {
+  console.log(req.params);
+  res.json(req.params);
+});
+
+app.post("/products/:title", (req, res) => {
+  console.log(req.params);
+  res.json(req.params);
+});
+
+app.get("/register", (req, res) => {
+  const filePath = path.join(__dirname, "/папка/register.html");
+  res.sendFile(filePath);
+});
+
+app.post("/register", (req, res) => {
+  const filePath = path.join(__dirname, "users.json");
+  try {
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "[]");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) throw err;
+
+    const users = JSON.parse(data);
+
+    if (users.length > 0) {
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email === req.body.email) {
+          res.send(
+            `<h1>Пользователь с email ${req.body.email} уже существует.</h1>`
+          );
+          return;
+        }
+      }
+    }
+
+    users.push(req.body);
+
+    fs.writeFile(filePath, JSON.stringify(users), (err) => {
+      if (err) throw err;
+      res.send("<h1>Пользователь зарегистрирован.");
+    });
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Не найдено</h1>");
 });
